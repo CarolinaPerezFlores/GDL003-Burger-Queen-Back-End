e9c431d02ff98deff627288f72ec2762ad2ca248
karo0wk <karo0wk@gmail.com>
2019-10-12 20:43:22 -0500

    deplouycon mi méetodo

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const app = require('./app');
+const bodyParser = require('body-parser');
 
-async function init() {
-    await app.listen(process.env.PORT || 3000);
-    console.log('server on port 3000');
-}
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to Tlacuali application. Take orders quickly. Organize and keep track of all your orders."});
+});
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
 
-init()
+
