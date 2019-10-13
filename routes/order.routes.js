6c3b3089dedac58ca443a3cf9b08b4db0de772c5
karo0wk <karo0wk@gmail.com>
2019-10-12 22:07:44 -0500

    se añaden todos los métodas de orders

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -4,6 +4,20 @@ module.exports = (app) => {
     const orders = require('../controllers/order.controllers');
 
     app.use(cors({credentials: true,}));
+ 
+    // Create a new Order
+    app.post('/orders', orders.create);
 
+    // Retrieve all Orders
     app.get('/orders', orders.findAll);
+
+    // Retrieve a single Order with orderId
+    app.get('/orders/:orderId', orders.findOne);
+
+    // Update a Order with orderId
+    app.put('/orders/:orderId', orders.update);
+
+    // Delete a Order with orderId
+    app.delete('/orders/:orderId', orders.delete);
+    
 }
