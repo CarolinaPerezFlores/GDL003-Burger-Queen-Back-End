5d9ec5b417d04fefa09dd50c83a3ce467b270e78
karo0wk <karo0wk@gmail.com>
2019-10-12 21:51:30 -0500

    se añade cors

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,5 +1,9 @@
+var cors = require('cors')
+
 module.exports = (app) => {
     const orders = require('../controllers/order.controllers');
+
+    app.use(cors({credentials: true,}));
 
     app.get('/orders', orders.findAll);
 }
