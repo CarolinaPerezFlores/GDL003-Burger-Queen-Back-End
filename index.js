bdbf887e4f04b122d01f28beba3c8597bb99d5b4
karo0wk <karo0wk@gmail.com>
2019-10-12 20:58:12 -0500

    welcome

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const app = require('./app');
-const bodyParser = require('body-parser');
+const express= require('express');
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.get('/', (req, res) => {
-    res.json({"message": "Welcome to Tlacuali application. Take orders quickly. Organize and keep track of all your orders."});
-});
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-});
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
+async function init() {
+    await app.listen(process.env.PORT || 3000);
+    console.log('server on port 3000');
 
+    app.get('/', (req, res) => {
+        res.json({"message": "Welcome to Tlacuali application. Take orders quickly. Organize and keep track of all your orders."});
+    });
+}
+
+init()
